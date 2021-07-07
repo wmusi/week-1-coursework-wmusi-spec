8cabc01256f24b6ddf187c360a6ede57e4314a31
wmusi <62408479+wmusi@users.noreply.github.com>
2021-07-06 23:00:31 -0400

    Update Week1.py
    added the __str__ interface

--- Week1.py
+++ Week1.py
@@ -1,21 +1,29 @@
 # week-1-coursework-wmusi-spec
 class Teams:
     def __init__(self,members):
-            self.__myTeam=members
+        self.__myTeam=members
+    #len interface implementation
     def __len__(self):
         return len(self.__myTeam)
+    # contain interface inplemention
     def __contains__(self, member):
         #print("in is from here only")
-        return member in self.__myTeam
+        return member.upper() in (iu.upper() for iu in self.__myTeam) #return a boolean value
+    #iter interface implementation
     def __iter__(self):
         #print("Iterator")
         return iter(self.__myTeam)
+    #string interface implementation
+    def __str__ (self):
+        return '\n'.join(self.__myTeam) #generate a joined list of membersin string format
 def main():
     classmates=Teams(['John','Steve','Tim'])
     print('Tim' in classmates)
     print('Sam' in classmates)
     iterator=iter(classmates)#we get iterable object reference
     for member in iterator:
-        print(member,end=" ")
+        print(member)
+    print("List of members in the team using print")
+    print(classmates)
 main()
 
